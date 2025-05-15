4d1f04d3d1c9c5a5caccd9bfcec283eb4b087b7c
Keerthi@012 <keerthi@ckpsoftware.com>
2025-05-15 15:58:03 +0530

    setting changed

--- student_alerts_app/settings.py
+++ student_alerts_app/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alert-app-gnhef4afbtgmdud4.centralindia-01.azurewebsites.net']
+
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
