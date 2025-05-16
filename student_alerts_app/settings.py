538e7fbb86a5e2f7a774ca1f0d0ada5e574fd1db
Keerthi@012 <keerthi@ckpsoftware.com>
2025-05-16 15:27:58 +0530

    do some 1-2 commit

--- student_alerts_app/settings.py
+++ student_alerts_app/settings.py
@@ -34,7 +34,13 @@ CSRF_TRUSTED_ORIGINS = [
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['message-alert-app-cbcwg4g2dffcgxfj.centralindia-01.azurewebsites.net', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'message-alert-app-cbcwg4g2dffcgxfj.centralindia-01.azurewebsites.net',
+    '169.254.132.3',
+    'localhost',
+    '127.0.0.1'
+]
+
 
 
 # Application references
